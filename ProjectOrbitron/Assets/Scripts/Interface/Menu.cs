9cb9061922eb512b0034f03e4e9124ecf8e5d124
JordanRE <jordan.rob997@gmail.com>
2023-12-11 17:24:28 -0300

    Menu Disc
    No terminado, ajustar lanzamiento

---
+++ ProjectOrbitron/Assets/Scripts/Interface/Menu.cs
@@ -0,0 +1,82 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using static UnityEngine.Rendering.DebugUI;
+
+public class Menu : MonoBehaviour
+{
+    private Vector3 objectToLookPosition;
+    public GameObject objectToLook;
+    public GameObject objectThatLook;
+    public GameObject scrollBar;
+    public GameObject rightButton;
+    public GameObject leftButton;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scrollBar.GetComponent<Scrollbar>().value = 0;
+        objectToLook = GameObject.FindGameObjectWithTag("MainCamera");
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        LookAtObject();
+        UpdateColorButton();
+    }
+    public void LookAtObject()
+    {
+        objectToLookPosition = objectToLook.transform.position;
+        objectToLookPosition.y = objectThatLook.transform.position.y;
+        objectThatLook.transform.LookAt(objectToLookPosition);
+    }
+
+    public void SetScrollBarValue(float value)
+    {
+        scrollBar.GetComponent<Scrollbar>().value = value;
+    }
+
+    public void UpdateColorButton()
+    {
+        if(scrollBar.GetComponent<Scrollbar>().value <= 0.1f)
+        {
+            leftButton.GetComponent<Image>().color = Color.gray;
+            rightButton.GetComponent<Image>().color = Color.white;
+        }else if(scrollBar.GetComponent<Scrollbar>().value >= 0.9f)
+        {
+            leftButton.GetComponent<Image>().color = Color.white;
+            rightButton.GetComponent<Image>().color = Color.gray;
+        }
+    }
+
+    public void HealthButton(float  amount)
+    {
+        float healthPlayer; 
+        healthPlayer = player.GetComponent<DynamicTarget>().GetHealth() + amount;
+        player.GetComponent<DynamicTarget>().SetHealth(healthPlayer);   
+    }
+
+    public void MicroHealthbutton(float amount, int cycles)
+    {
+        for(int i = 0; i < cycles; i++)
+        {
+            float healthPlayer;
+            healthPlayer = player.GetComponent<DynamicTarget>().GetHealth() + amount;
+            player.GetComponent<DynamicTarget>().SetHealth(healthPlayer);
+        }
+    }
+    public void LoadTitle()
+    {
+        SceneManager.LoadScene("Title");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ ProjectOrbitron/Assets/Scripts/ShootDisc.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ShootDisc : MonoBehaviour
+{
+    public Transform tip;
+    public GameObject disc;
+    public float speed;
+    public InputActionReference shootInputAction;
+    public InputActionReference shootInputActionSim;
+    private bool shooting = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        shooting = shootInputAction.action.IsPressed();
+ 
+    }
+    void FixedUpdate()
+    {
+        if (shooting)
+        {
+            print("DIIISSCCC");
+            shootDisc();
+        }
+    }
+
+    public void shootDisc()
+    {
+        GameObject clone = Instantiate(disc, tip.position, Quaternion.identity);
+        Rigidbody rb = clone.GetComponent<Rigidbody>();
+        rb.AddForce(tip.forward * speed, ForceMode.Impulse);
+    }
+}
